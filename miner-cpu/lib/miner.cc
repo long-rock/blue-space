af223b041f4cce3a1383183834c62355c8616344
Francesco Ceccon <francesco@ceccon.me>
2021-08-15 21:55:33 +0100

    feat: use mpz directly

--- miner-cpu/lib/miner.cc
+++ miner-cpu/lib/miner.cc
@@ -17,16 +17,6 @@ using namespace miner::cpu;
 namespace internal
 {
 
-mpz_class wrap_coordinate(int64_t c)
-{
-    if (c >= 0)
-    {
-        return mpz_class(c);
-    }
-    mpz_class t(-c);
-    return P - t;
-}
-
 } // namespace internal
 
 CpuMiner::CpuMiner(const CpuMinerOptions &options) : options_(options)
@@ -44,37 +34,56 @@ void CpuMiner::mine(const common::ChunkFootprint &chunk, uint32_t rarity, uint32
     mpz_class rarity_(rarity);
     mpz_class threshold = P / rarity_;
 
-    Sponge sponge;
+    // realloc to appropriate bit size
+    hash::realloc_mpz(key_.get_mpz_t());
+    hash::realloc_mpz(rarity_.get_mpz_t());
+    hash::realloc_mpz(threshold.get_mpz_t());
+
     if (options_.num_threads > 0)
     {
         BOOST_LOG_TRIVIAL(info) << "Set OMP CPU threads to " << options_.num_threads;
         omp_set_num_threads(options_.num_threads);
     }
 
-    miner::cpu::Sponge();
-#pragma omp parallel for default(none) shared(chunk, result, threshold, key_) private(sponge)
-    for (int64_t x = chunk.bottom_left.x; x < chunk.bottom_left.x + chunk.side_length; ++x)
+    mpz_t x_, y_, planet_hash;
+    hash::Sponge sponge;
+
+#pragma omp parallel default(none)                                                                                     \
+    shared(chunk, result, threshold, key_) private(sponge, x_, y_, planet_hash)
     {
-        mpz_class x_ = internal::wrap_coordinate(x);
-        sponge.reset();
-        sponge.inject(x_.get_mpz_t());
-        sponge.mix(key_.get_mpz_t());
-        sponge.save();
-        for (int64_t y = chunk.bottom_left.y; y < chunk.bottom_left.y + chunk.side_length; ++y)
+        // initialize thread private variables once before loop
+        hash::init_mpz(x_);
+        hash::init_mpz(y_);
+        hash::init_mpz(planet_hash);
+
+#pragma omp for
+        for (int64_t x = chunk.bottom_left.x; x < chunk.bottom_left.x + chunk.side_length; ++x)
         {
-            mpz_class planet_hash;
-            sponge.restore();
-            mpz_class y_ = internal::wrap_coordinate(y);
-            sponge.inject(y_.get_mpz_t());
+            hash::wrap_coordinate(x_, x);
+            sponge.reset();
+            sponge.inject(x_);
             sponge.mix(key_.get_mpz_t());
-            sponge.result(planet_hash.get_mpz_t());
-            auto is_planet = miner::cpu::is_planet(planet_hash.get_mpz_t(), threshold.get_mpz_t());
-            if (is_planet)
+            sponge.save();
+            for (int64_t y = chunk.bottom_left.y; y < chunk.bottom_left.y + chunk.side_length; ++y)
             {
-                Coordinate coord(x, y);
-                std::string hash = planet_hash.get_str();
-                PlanetLocation location(std::move(coord), std::move(hash));
-                result.push_back(location);
+                hash::wrap_coordinate(y_, y);
+                sponge.restore();
+                sponge.inject(y_);
+                sponge.mix(key_.get_mpz_t());
+                sponge.result(planet_hash);
+                auto is_planet = miner::cpu::hash::is_planet(planet_hash, threshold.get_mpz_t());
+                if (is_planet)
+                {
+                    Coordinate coord(x, y);
+                    mpz_class hash_temp(planet_hash);
+                    std::string hash = hash_temp.get_str();
+                    PlanetLocation location(std::move(coord), std::move(hash));
+#pragma omp critical
+                    {
+                        // std vector is not thread safe
+                        result.push_back(location);
+                    }
+                }
             }
         }
     }
