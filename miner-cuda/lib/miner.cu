c48e5a6f446245f4bfe20a9ad78c1b52b74421f0
Francesco Ceccon <francesco@ceccon.me>
2021-08-21 21:41:14 +0100

    fix: incorrect value with block size != 1

--- miner-cuda/lib/miner.cu
+++ miner-cuda/lib/miner.cu
@@ -29,7 +29,7 @@ CudaMiner::~CudaMiner()
 namespace kernel
 {
 
-static const uint32_t BIT_SIZE = 512;
+static const uint32_t BIT_SIZE = 256;
 static const uint32_t C_SIZE = 219;
 
 typedef cgbn_mem_t<BIT_SIZE> bn_mem_t;
@@ -87,38 +87,41 @@ __device__ __forceinline__ void wrap_coordinate(env_t &env, bn_t &c_bn, int64_t 
     typename env_t::cgbn_t n, m;
     if (c >= 0)
     {
-        env.set_ui32(n, c);
+        uint32_t c_ = static_cast<uint32_t>(c);
+        env.set_ui32(n, c_);
         env.set(c_bn, n);
         return;
     }
-    env.set_ui32(m, -c);
+    uint32_t c_ = static_cast<uint32_t>(-c);
+    env.set_ui32(m, c_);
     env.sub(n, p, m);
     env.set(c_bn, n);
 }
 
 template <class env_t, class bn_t = typename env_t::cgbn_t>
-__device__ __forceinline__ void field_add(env_t &env, bn_t &r, const bn_t &a, const bn_t &b, bn_t &t, const bn_t &p)
+__device__ __forceinline__ void field_add(env_t &env, bn_t &r, const bn_t &a, const bn_t &b, const bn_t &p)
 {
     env.add(r, a, b);
-    while (env.compare(r, p) > 0) {
+    while (env.compare(r, p) > 0)
+    {
         env.sub(r, r, p);
     }
 }
 
-template <class env_t, class bn_t = typename env_t::cgbn_t>
-__device__ __forceinline__ void field_mul(env_t &env, bn_t &r, const bn_t &a, const bn_t &b, bn_t &t, const bn_t &p)
+template <class env_t, class bn_t = typename env_t::cgbn_t, class bn_wide_t = typename env_t::cgbn_wide_t>
+__device__ __forceinline__ void field_mul(env_t &env, bn_t &r, const bn_t &a, const bn_t &b, const bn_t &p)
 {
-    env.mul(r, a, b);
-    env.rem(r, r, p);
+    bn_wide_t w;
+    env.mul_wide(w, a, b);
+    env.rem_wide(r, w, p);
 }
 
 template <class env_t, class bn_t = typename env_t::cgbn_t>
-__device__ __forceinline__ void fifth_power(env_t &env, bn_t &r, const bn_t &n, bn_t &s, bn_t &f, bn_t &t,
-                                            const bn_t &p)
+__device__ __forceinline__ void fifth_power(env_t &env, bn_t &r, const bn_t &n, bn_t &s, bn_t &f, const bn_t &p)
 {
-    field_mul(env, s, n, n, t, p);
-    field_mul(env, f, s, s, t, p);
-    field_mul(env, r, f, n, t, p);
+    field_mul(env, s, n, n, p);
+    field_mul(env, f, s, s, p);
+    field_mul(env, r, f, n, p);
 }
 
 template <class env_t, class bn_t = typename env_t::cgbn_t> class Sponge
@@ -132,23 +135,23 @@ template <class env_t, class bn_t = typename env_t::cgbn_t> class Sponge
 
     __device__ void inject(env_t &env, const bn_t &x, const bn_t &P)
     {
-        field_add(env, l_, l_, x, t0_, P);
+        field_add(env, l_, l_, x, P);
     }
 
     __device__ void mix(env_t &env, const bn_t &key, const bn_t *C, std::size_t C_size, const bn_t &P)
     {
         for (uint32_t j = 0; j < C_size; ++j)
         {
-            field_add(env, t0_, key, l_, t1_, P);
-            field_add(env, t1_, t0_, C[j], t2_, P);
-            fifth_power(env, t0_, t1_, t2_, t3_, t4_, P);
-            field_add(env, t1_, t0_, r_, t2_, P);
+            field_add(env, t0_, key, l_, P);
+            field_add(env, t1_, t0_, C[j], P);
+            fifth_power(env, t0_, t1_, t2_, t3_, P);
+            field_add(env, t1_, t0_, r_, P);
             env.set(r_, l_);
             env.set(l_, t1_);
         }
-        field_add(env, t0_, key, l_, t2_, P);
-        fifth_power(env, t1_, t0_, t2_, t3_, t4_, P);
-        field_add(env, t0_, t1_, r_, t2_, P);
+        field_add(env, t0_, key, l_, P);
+        fifth_power(env, t1_, t0_, t2_, t3_, P);
+        field_add(env, t0_, t1_, r_, P);
         env.set(r_, t0_);
     }
 
@@ -225,7 +228,7 @@ __global__ void mine_batch_kernel(cgbn_error_report_t *report, const ChunkFootpr
     sponge.mix(env, key, C, C_SIZE, P);
     sponge.save(env);
 
-    uint32_t start_size_y = blockIdx.y * blockDim.y + items_per_thread * threadIdx.y;
+    uint32_t start_size_y = blockIdx.y * (blockDim.y + items_per_thread) + items_per_thread * threadIdx.y;
     for (uint32_t i = 0; i < items_per_thread; ++i)
     {
         if (start_size_y + i >= chunk.side_length)
@@ -234,15 +237,17 @@ __global__ void mine_batch_kernel(cgbn_error_report_t *report, const ChunkFootpr
         }
 
         int64_t coord_y = chunk.bottom_left.y + start_size_y + i;
+
         sponge.restore(env);
         wrap_coordinate(env, yi, coord_y, P);
         sponge.inject(env, yi, P);
         sponge.mix(env, key, C, C_SIZE, P);
         sponge.result(env, hash);
 
-        uint32_t result_idx = blockIdx.x * chunk.side_length + start_size_y + i;
-        env.store(&(result[result_idx].hash), hash);
+        uint32_t result_idx = blockIdx.x + chunk.side_length * (start_size_y + i);
         result[result_idx].is_planet = env.compare(hash, planet_threshold) < 0;
+        // result[result_idx].is_planet = true;
+        env.store(&(result[result_idx].hash), hash);
         result[result_idx].x = coord_x;
         result[result_idx].y = coord_y;
     }
@@ -266,6 +271,9 @@ void run_mine_batch(int device, const CudaMinerOptions &options, const ChunkFoot
     uint32_t items_per_block = options.thread_work_size * options.block_size;
     // grid_size_y = ceil(side_length / items_per_block)
     uint32_t grid_size_y = (chunk.side_length + items_per_block - 1) / items_per_block;
+    if (options.block_size != 1) {
+        throw std::runtime_error("block_size must be 1");
+    }
     dim3 block_size(bn_params::TPI, options.block_size);
     dim3 grid_size(chunk.side_length, grid_size_y);
 
@@ -298,6 +306,7 @@ void run_mine_batch(int device, const CudaMinerOptions &options, const ChunkFoot
             to_mpz(planet_hash.get_mpz_t(), cpu_batch[i].hash);
             Coordinate coord(cpu_batch[i].x, cpu_batch[i].y);
             std::string hash = planet_hash.get_str();
+            BOOST_LOG_TRIVIAL(info) << coord.x << ", " << coord.y << " = " << hash;
             PlanetLocation location(std::move(coord), std::move(hash));
             result.push_back(location);
         }
@@ -349,14 +358,19 @@ void CudaMiner::mine(const ChunkFootprint &chunk, uint32_t rarity, uint32_t key,
     mpz_class key_mpz(key);
     kernel::from_mpz(key_mpz.get_mpz_t(), key_bn);
 
-    switch (options_.threads_per_item) {
-        case ThreadsPerItem::TPI_4:
-            return kernel::run_mine_batch<kernel::BnParams<4>>(device_, options_, chunk, planet_threshold_bn, key_bn, result);
-        case ThreadsPerItem::TPI_8:
-            return kernel::run_mine_batch<kernel::BnParams<8>>(device_, options_, chunk, planet_threshold_bn, key_bn, result);
-        case ThreadsPerItem::TPI_16:
-            return kernel::run_mine_batch<kernel::BnParams<16>>(device_, options_, chunk, planet_threshold_bn, key_bn, result);
-        default:
-            return kernel::run_mine_batch<kernel::BnParams<32>>(device_, options_, chunk, planet_threshold_bn, key_bn, result);
+    switch (options_.threads_per_item)
+    {
+    case ThreadsPerItem::TPI_4:
+        return kernel::run_mine_batch<kernel::BnParams<4>>(device_, options_, chunk, planet_threshold_bn, key_bn,
+                                                           result);
+    case ThreadsPerItem::TPI_8:
+        return kernel::run_mine_batch<kernel::BnParams<8>>(device_, options_, chunk, planet_threshold_bn, key_bn,
+                                                           result);
+    case ThreadsPerItem::TPI_16:
+        return kernel::run_mine_batch<kernel::BnParams<16>>(device_, options_, chunk, planet_threshold_bn, key_bn,
+                                                            result);
+    default:
+        return kernel::run_mine_batch<kernel::BnParams<32>>(device_, options_, chunk, planet_threshold_bn, key_bn,
+                                                            result);
     }
 }
